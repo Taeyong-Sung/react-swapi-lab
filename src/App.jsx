b85d9b24ab25e01b5b4b9c90f65604210cc358a5
Taeyong Sung <canny0201@gmail.com>
2024-08-27 02:33:02 -0400

    lab done

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,63 @@
 // npm modules
-// import { useState } from 'react'
 import { useState, useEffect } from 'react'
-import { Routes, Route, Link } from 'react-router-dom'
-
-
-import './App.css'
+import { Link, Routes, Route, useParams } from 'react-router-dom'
 
 // services
-import { getAllStarShips } from './services/sw-api'
+import { getAllStarShips} from './services/sw-api'
 
+const StarShipsList = ({ starShips }) => (
+  <div className="starShip-container">
+    <nav>STAR WARS STARSHIPS</nav>
+    <main>
+      {starShips.map((starShip, index) => (
+        <div className="starShip-card" key={index}>
+          <Link to={`/starships/${index}`}>
+            {starShip.name}
+          </Link>
+        </div>
+      ))}
+    </main>
+  </div>
+)
+
+const StarShipDetail = ({ starShips }) => {
+  const { starShipId } = useParams()
+  const starShip = starShips[starShipId]
+
+  return (
+    <div className="starShip-detail">
+      <h3>NAME: {starShip.name}</h3>
+      <h3>MODEL: {starShip.model}</h3>
+      <Link to="/">Return to Starship List</Link>
+    </div>
+  )
+}
 
 const App = () => {
   const [starShips, setStarShips] = useState([])
 
   useEffect(() => {
     const fetchStarShips = async () => {
-      const {results} = await getAllStarShips()
+      const { results } = await getAllStarShips()
       setStarShips(results)
     }
     fetchStarShips()
   }, [])
 
+  if (!starShips.length) return <h1>Loading…</h1>
 
   return (
-  <>
-    <nav>STAR WARS STARSHIPS</nav>
-    <main className="starShip-list">
-      {starShips.map(starShip =>
-        <div className="link-container" key={starShip.url}>
-            {starShip.name}
-        </div>
-      )}
-    </main>
-  </>
-  );
+    <Routes>
+      <Route 
+      path="/" 
+      element={<StarShipsList 
+      starShips={starShips} />} />
+      <Route 
+      path="/starships/:starShipId" 
+      element={<StarShipDetail 
+      starShips={starShips} />} />
+    </Routes>
+  )
 }
 
-export default App;
+export default App
